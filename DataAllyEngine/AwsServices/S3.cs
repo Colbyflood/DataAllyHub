3c9ece6fa1b6c7e2e199ca2eb46d7055cac9b7dc
Chris <laforet@chrislaforetsoftware.com>
2025-04-03 20:26:46 -0400

    Continued buildout of server core functions

---
+++ DataAllyEngine/AwsServices/S3.cs
@@ -0,0 +1,34 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace DataAllyEngine.AwsServices;
+
+public class S3
+{
+    public static async Task<string> ReadTextFile(string bucketName, string fileName)
+    {
+        var s3Client = new AmazonS3Client(RegionEndpoint.USEast1);
+
+        try
+        {
+            var request = new GetObjectRequest
+            {
+                BucketName = bucketName,
+                Key = fileName
+            };
+
+            using var response = await s3Client.GetObjectAsync(request);
+            using var reader = new StreamReader(response.ResponseStream);
+            return await reader.ReadToEndAsync();
+        }
+        catch (AmazonS3Exception e)
+        {
+            throw new Exception($"Amazon S3 error on server. Message:'{e.Message}' when writing an object");
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Unknown error on server. Message:'{e.Message}' when writing an object");
+        }
+    }
+}
---
+++ FacebookLoader/Common/FacebookParameters.cs
@@ -0,0 +1,26 @@
+﻿namespace FacebookLoader.Common;
+
+public class FacebookParameters
+{
+	public readonly string accountId;
+	public readonly string token;
+	public readonly string facebookVersion;
+
+	public FacebookParameters(string accountId, string token, string facebookVersion = "22.0")
+	{
+		this.accountId = accountId;
+		this.token = token;
+		this.facebookVersion = facebookVersion;
+	}
+
+	public string CreateUrlFor(string endpoint)
+	{
+		var accountValue = accountId;
+		if (!accountValue.StartsWith("act_"))
+		{
+			accountValue = $"act_{accountId}";
+		}
+
+		return $"https://graph.facebook.com/v{facebookVersion}/{accountValue}/{endpoint}";
+	}
+}
