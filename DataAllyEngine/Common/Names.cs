085780c5b8dbcf1a45b2d89cfa62fe385cc168ed
ChrisLaforet <laforet@chrislaforetsoftware.com>
2025-04-08 14:00:00 -0400

    [Broken build] Attempting to reason out loader tasks

--- DataAllyEngine/Common/Names.cs
+++ DataAllyEngine/Common/Names.cs
@@ -2,5 +2,13 @@
 
 public class Names
 {
+    // ReSharper disable InconsistentNaming
     public const string DB_CONNECTION_STRING_KEY = "DataAllyDatabase";
+
+    public const string FEED_TYPE_AD_CREATIVE = "AdCreative";
+    public const string FEED_TYPE_AD_INSIGHT = "AdInsight";
+    public const string FEED_TYPE_AD_IMAGE = "AdImage";
+
+    public const string SCOPE_TYPE_DAILY = "Daily";
+    public const string SCOPE_TYPE_BACKFILL = "Backfill";
 }
