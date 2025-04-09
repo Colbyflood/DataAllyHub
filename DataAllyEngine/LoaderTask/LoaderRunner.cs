cfed8ce99f28fee0bb810f532fb3b04b72c67566
ChrisLaforet <laforet@chrislaforetsoftware.com>
2025-04-09 13:30:01 -0400

    [Broken code] Added loader runner tasks for AdInsights

--- DataAllyEngine/LoaderTask/LoaderRunner.cs
+++ DataAllyEngine/LoaderTask/LoaderRunner.cs
@@ -26,22 +26,34 @@ public class LoaderRunner
 		Task.Run(() => StartAdCreativesLoadTask(facebookParameters, channel, scopeType, loaderProxy, logging));
 	}
 
-	public void ResumeAdImagesLoad(FacebookParameters facebookParameters, FbRunLog runlog, string url)
+	public void ResumeAdCreativesLoad(FacebookParameters facebookParameters, FbRunLog runlog, string url)
 	{
 		Task.Run(() => ResumeAdCreativesLoadTask(facebookParameters, runlog, url, loaderProxy, logging));
 	}
 
-
-	public void StartAdImagessLoad(FacebookParameters facebookParameters, Channel channel, string scopeType)
+	public void StartAdImagesLoad(FacebookParameters facebookParameters, Channel channel, string scopeType)
 	{
 		Task.Run(() => StartAdImagesLoadTask(facebookParameters, channel, scopeType, loaderProxy, logging));
 	}
 
-	public void ResumeAdCreativesLoad(FacebookParameters facebookParameters, FbRunLog runlog, string url)
+	public void ResumeAdImagesLoad(FacebookParameters facebookParameters, FbRunLog runlog, string url)
 	{
 		Task.Run(() => ResumeAdImagesLoadTask(facebookParameters, runlog, url, loaderProxy, logging));
 	}
+	
+	public void StartAdInsightsLoad(FacebookParameters facebookParameters, Channel channel, string scopeType, DateTime startDate, DateTime endDate)
+	{
+		Task.Run(() => StartAdInsightsLoadTask(facebookParameters, channel, scopeType, startDate, endDate, loaderProxy, logging));
+	}
 
+	public void ResumeAdInsightsLoad(FacebookParameters facebookParameters, FbRunLog runlog, string url)
+	{
+		Task.Run(() => ResumeAdInsightsLoadTask(facebookParameters, runlog, url, loaderProxy, logging));
+	}
+
+	
+	// Tasks to run in their own threads
+	
 	private static async Task StartAdCreativesLoadTask(FacebookParameters facebookParameters, Channel channel, string scopeType, ILoaderProxy loaderProxy, ILogging logger)
 	{
 		logger.LogInformation($"Starting AdCreativeLoadTask for channel {channel.Id}");
@@ -84,6 +96,29 @@ public class LoaderRunner
 		var service = new FacebookAdImagesService(channel, loaderProxy, facebookParameters, logger);
 		await service.ResumeAdImagesLoad(runlog, url);
 		logger.LogInformation($"Exiting resumed AdImageLoadTask for channel {runlog.ChannelId}");
+	}
+	
+	private static async Task StartAdInsightsLoadTask(FacebookParameters facebookParameters, Channel channel, string scopeType, 
+		DateTime startDate, DateTime endDate, ILoaderProxy loaderProxy, ILogging logger)
+	{
+		logger.LogInformation($"Starting AdInsightLoadTask for channel {channel.Id} between {startDate} and {endDate}");
+		var service = new FacebookAdInsightsService(channel, loaderProxy, facebookParameters, logger);
+		await service.InitiateAdInsightsLoad(scopeType, startDate, endDate);
+		logger.LogInformation($"Exiting started AdInsightLoadTask for channel {channel.Id}");
+	}
+	
+	private static async Task ResumeAdInsightsLoadTask(FacebookParameters facebookParameters, FbRunLog runlog, string url, ILoaderProxy loaderProxy, ILogging logger)
+	{
+		logger.LogInformation($"Resuming AdInsightLoadTask for channel {runlog.ChannelId}");
+		var channel = loaderProxy.GetChannelById(runlog.ChannelId);
+		if (channel == null)
+		{
+			logger.LogError($"Resuming AdImageLoadTask for channel {runlog.ChannelId} failed because channel {runlog.ChannelId} not found");
+			return;
+		}
+		var service = new FacebookAdInsightsService(channel, loaderProxy, facebookParameters, logger);
+		await service.ResumeAdInsightsLoad(runlog, url);
+		logger.LogInformation($"Exiting resumed AdInsightLoadTask for channel {runlog.ChannelId}");
 	}
 
 }
