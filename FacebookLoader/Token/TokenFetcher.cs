3a6fcb5a19857988a8fb0baa7ab44068a020248a
Chris <laforet@chrislaforetsoftware.com>
2025-06-02 14:10:54 -0400

    [Broken build] Working out fetching of tokens from FB

--- FacebookLoader/Token/TokenFetcher.cs
+++ FacebookLoader/Token/TokenFetcher.cs
@@ -1,18 +1,96 @@
+using System.Net.Http.Headers;
+using FacebookLoader.Common;
 using FacebookLoader.Content;
+using Newtonsoft.Json.Linq;
 
 namespace FacebookLoader.Token;
 
 public class TokenFetcher
 {
-	public static FacebookAccount GetFacebookAccountForToken(string token)
+	// ReSharper disable once InconsistentNaming
+	private const int SOCKET_TIMEOUT_SECONDS = 30;
+	
+	public static async Task<FacebookAccount> GetFacebookAccountForToken(FacebookParameters facebookParameters, string token)
 	{
-		
+		const string FieldsList = "id,name";
+			
+		using var httpClient = new HttpClient()
+		{
+			Timeout = TimeSpan.FromSeconds(SOCKET_TIMEOUT_SECONDS)
+		};
+		httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+		HttpResponseMessage? response = null;
+		try
+		{
+			string url = $"{facebookParameters.CreateUrlFor("ads")}?fields={FieldsList}&access_token={token}";
+
+			response = await httpClient.GetAsync(url);
+			response.EnsureSuccessStatusCode();
+
+			var responseContent = await response.Content.ReadAsStringAsync();
+			return JObject.Parse(responseContent);
+		}
+		catch (HttpRequestException httpEx) when (httpEx.StatusCode.HasValue)
+		{
+			//var responseText = httpEx.Message;
+			var responseText = string.Empty;
+
+			if (response != null)
+			{
+				responseText = await response.Content.ReadAsStringAsync();
+			}
+
+			Console.Error.WriteLine($"HTTP error occurred: {httpEx} while calling graph api");
+			throw new FacebookHttpException((int)httpEx.StatusCode.Value, responseText);
+		}
+		catch (Exception ex)
+		{
+			Console.Error.WriteLine($"An error occurred: {ex} while calling graph api");
+			throw new Exception($"Other error occurred: {ex.Message}");
+		}
 	}
 	
-	public List<FacebookPageToken> GetFacebookPageTokensForAccount(string accountNumber, string token)
+	public static async Task<List<FacebookPageToken>> GetFacebookPageTokensForAccount(FacebookParameters facebookParameters, string accountNumber, string token)
 	{
-		// This method should return a list of FacebookPageToken objects for the given account number and token.
-		// The implementation would typically involve making an API call to Facebook to retrieve the tokens.
+		const string FieldsList = "id,name,access_token";
+
+		using var httpClient = new HttpClient()
+		{
+			Timeout = TimeSpan.FromSeconds(SOCKET_TIMEOUT_SECONDS)
+		};
+		httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+		HttpResponseMessage? response = null;
+		try
+		{
+			string url = $"{facebookParameters.CreateUrlFor("ads")}?fields={FieldsList}&access_token={token}";
+
+			response = await httpClient.GetAsync(url);
+			response.EnsureSuccessStatusCode();
+
+			var responseContent = await response.Content.ReadAsStringAsync();
+			return JObject.Parse(responseContent);
+		}
+		catch (HttpRequestException httpEx) when (httpEx.StatusCode.HasValue)
+		{
+			//var responseText = httpEx.Message;
+			var responseText = string.Empty;
+
+			if (response != null)
+			{
+				responseText = await response.Content.ReadAsStringAsync();
+			}
+
+			Console.Error.WriteLine($"HTTP error occurred: {httpEx} while calling graph api");
+			throw new FacebookHttpException((int)httpEx.StatusCode.Value, responseText);
+		}
+		catch (Exception ex)
+		{
+			Console.Error.WriteLine($"An error occurred: {ex} while calling graph api");
+			throw new Exception($"Other error occurred: {ex.Message}");
+		}
+	
 		return new List<FacebookPageToken>();
 	}
 }
